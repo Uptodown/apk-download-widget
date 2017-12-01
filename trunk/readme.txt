ebca2585b17fb9cedc703c769f7c417295ccf271
Luis Hernández <luishernandezgarrido@gmail.com>
2017-12-01 12:37:01 +0100

    First Commit
    Full trunk directory from SVN

---
+++ trunk/readme.txt
@@ -0,0 +1,54 @@
+﻿=== Uptodown APK Download Widget ===
+
+Contributors: uptodown
+Tags: Apps, APK, Android, Download
+Requires at least: 3.0.1
+Tested up to: 4.7.2
+Stable tag: 0.1.1
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Add information about any Android app (+3M Apps) right onto your blog and mobile site along with info on APK downloads
+
+== Description ==
+
+This responsive widget allows your users to access and download APKs easily from different sources (Google Play and Uptodown). It’s ready to use as a WordPress widget via shortcode along with the Package Name for the App.
+
+Uptodown is a major marketplace across the globe. Our objective is to offer all our content to users as quickly and comfortably as possible.  We’re not the only ones out there. Google Play is the largest app store in the world. But, we aren’t a store. Taking over where they’re missing out, we offer a different service by supplying users with APK downloads for their apps. Along with these advantages we also provide: installs for obsolete versions of apps, safer back ups, and fewer or no hardware restrictions, etc.
+
+== Installation ==
+
+Using our plugin is easy, just install it, activate and use the short code to add detailed information to your post about any Android App. 
+
+1. Upload the plugin files to the `/wp-content/plugins/plugin-name` directory, or install the plugin through the WordPress plugins screen directly.
+2. Activate the plugin through the 'Plugins' screen in WordPress
+3. Inserting our widget onto any part of your blog post or page is as easy as typing in the following code anywhere within your text (you can also directly paste it when editing)
+
+[utd-widget packagename='package name prefix']
+
+Our plugin detects your WordPress language and delivers the widget automatically into your own language (if available).
+
+== Frequently Asked Questions ==
+
+= How can I get the Package Name of any App =
+
+You can access easily to this information on Uptodown.com. Just look for the App and go to “more information” section on its information page. Example: http://whatsapp-messenger.en.uptodown.com/android
+
+Scroll down to: More info -> Package Name
+
+== Screenshots ==
+
+1. Result of your widget on a mobile screen screenshot-1.png 
+2. An example of your Uptodown WP Widget on Desktop screenshot-2.jpg
+
+== Changelog ==
+
+= 0.1.1 =
+* SSL/TLS Support. Now the widget will work correctly on https URLs
+
+= 0.1.0 =
+* Automatic detection of Apps on your posts. Now, the Uptodown widget is able to add the APK download to any App already linked on your post to Google Play. This feature is compatible with the shortcode +package_name, which will have priority over the automatic app detection.
+* New plugin config page that disables the automatic app detection on your post if needed.
+
+= 0.0.1 =
+* First public version
---
+++ trunk/uptodown_wp_widget.php
@@ -0,0 +1,226 @@
+<?php
+
+/*
+Plugin Name: Uptodown Wordpress Widget
+Plugin URI: http://www.uptodown.com
+Description: Add information about any Android app right onto your blog
+Version: 0.1.1
+Author: Uptodown
+Author URI: http://www.uptodown.com
+*/
+
+class UptodownWPShortcode
+{
+	private $options;
+	private $shortcode_exist = false;
+	
+    public function __construct() {
+		$this->check_options();
+        add_shortcode('utd-widget', array($this,'shortcode_translator'));
+        add_action('wp_enqueue_scripts', array($this,'enqueue_script'));
+		add_filter('the_content',array($this,'setWidgetByPSIDs'));
+    }
+	
+	public function check_options() {
+		$options = get_option('utd_widget');
+		if(empty($options)) {
+			$options = array(
+				'autopsids' => 1
+			);
+			add_option('utd_widget',$options);
+		}
+		$this->options = $options;
+	}
+
+    public function shortcode_translator($params) {
+        $opts = shortcode_atts(array(
+            'packagename' => '',
+        ), $params);
+		
+		$lang = $this->getAvailableLanguage(substr(get_bloginfo('language'),0,2));
+		update_option('utd_widget_lang', $lang);
+        
+        if(empty($opts['packagename'])) {
+            return '';
+        } else {
+			$shortcode_exist = true;
+            return '<div class="utd_widget" data-packagename="' . $opts['packagename'] . '"></div>';
+        }
+    }
+	
+	public function getAvailableLanguage($lang) {
+        $default = 'en';
+        if(empty($lang)) {
+            return $default;
+        }
+		$dictionary = array(
+			'en'=>'en',
+			'es'=>'es',
+			'br'=>'br',
+			'pt'=>'br',
+			'fr'=>'fr',
+			'de'=>'de',
+			'it'=>'it',
+			'cn'=>'cn',
+			'zh'=>'cn',
+			'jp'=>'jp',
+			'ar'=>'ar',
+			'ru'=>'ru',
+			'in'=>'in',
+			'ko'=>'ko',
+			'kr'=>'ko',
+			'tr'=>'tr',
+			'id'=>'id',
+			'th'=>'th',
+		);
+		$real = $dictionary[$lang];
+		if(empty($real)) {
+			return $default;
+		} else {
+			return $real;
+		}
+	}
+	
+    public function enqueue_script() {
+        wp_enqueue_script('utd-widget-js','//api.uptodown.com/' . get_option('utd_widget_lang','en') . '/widget/nopw/widget.js');
+    }
+	
+	public function setWidgetByPSIDs($content) {
+		if(!empty($this->options['autopsids']) && !has_shortcode($content,'utd-widget')) {
+			if($appids=$this->getPSIDs($content)) {
+				$widgets = '';
+				foreach($appids as $appid) {
+					$widgets .= '<div class="utd_widget" data-packagename="' . $appid . '"></div>';
+				}
+				$content .= $widgets;
+				$widget_appears = true;
+			}
+		}
+		return $content;
+	}
+	
+	public function getPSIDs($article) {
+        require_once('simplehtmldom/simple_html_dom.php');
+        $appids = array();
+        $text = new simple_html_dom();
+        $text->load($article);
+        $img = $text->find('img',0);
+        if(!empty($img) && !empty($img->src)) {
+            $screen = $img->src;
+        }
+        $links = $text->find('a');
+        foreach($links as $link) {
+            if(!empty($link->href) && strpos($link->href,'play.google.com/store/apps/details')!==false && !is_bool(strpos($link->href,'?'))) {
+                $query_string = substr($link->href,strpos($link->href,'?')+1);
+                parse_str($query_string, $params);
+                if(!in_array($params['id'],$appids)) {
+                    $appids[] = $params['id'];
+                }
+            }
+        }
+        return $appids;
+    }
+}
+
+class UptodownWPShortcodeSettings
+{
+    private $options;
+
+    public function __construct() {
+		$this->check_options();
+        add_action('admin_menu', array($this,'add_plugin_page'));
+        add_action('admin_init', array($this,'page_init'));
+		add_filter('plugin_action_links_' . plugin_basename(__FILE__), array($this,'my_plugin_action_links'));
+    }
+	
+	public function check_options() {
+		// Form
+		if(isset($_POST['utd_widget_options'])) {
+			$options = $_POST['utd_widget_options'];
+			if(!isset($options['autopsids'])) {
+				$options['autopsids'] = 0;
+			}
+			update_option('utd_widget',$options);
+			$this->options = get_option('utd_widget');
+		// Default / Install
+		} else {
+			$options = get_option('utd_widget');
+			if(empty($options)) {
+				$options = array(
+					'autopsids' => 1
+				);
+				add_option('utd_widget',$options);
+			}
+			$this->options = $options;
+		}
+	}
+	
+	public function my_plugin_action_links($links) {
+	   array_unshift($links,'<a href="'. esc_url( get_admin_url(null, 'options-general.php?page=uptodown-widget-settings') ) .'">Settings</a>');
+	   return $links;
+	}
+	
+    public function add_plugin_page() {
+        add_options_page( // add_menu_page
+            'Uptodown Widget', 
+            'Uptodown Widget', 
+            'manage_options',
+            'uptodown-widget-settings', 
+            array($this,'create_admin_page')
+        );
+    }
+	
+    public function create_admin_page() {
+        $this->options = get_option('utd_widget');
+        ?>
+        <div class="wrap">
+            <form method="post">
+            <?php
+                settings_fields( 'utd_widget_option_group' );
+                do_settings_sections( 'uptodown-widget-settings' );
+                submit_button();
+            ?>
+            </form>
+        </div>
+        <?php
+    }
+	
+    public function page_init() {        
+        register_setting(
+            'utd_widget_option_group', // Option group
+            'utd_widget', // Option name
+            null // Sanitize
+        );
+        add_settings_section(
+            'setting_section_id', // ID
+            'Uptodown Widget Settings', // Title
+            null, // Callback
+            'uptodown-widget-settings' // Page
+        );
+        add_settings_field(
+            'autopsids', 
+            'Autodetect Play Store links?', 
+            array( $this, 'autopsids_callback' ), 
+            'uptodown-widget-settings', 
+            'setting_section_id'
+        );      
+    }
+	
+    public function autopsids_callback() {
+		$checked = !empty($this->options['autopsids'])
+			? 'checked'
+			: ''
+		;
+        echo 
+            '<input type="checkbox" id="autopsids" name="utd_widget_options[autopsids]" value="1" ' . $checked . ' />'
+        ;
+    }
+}
+
+if(is_admin()) {
+	new UptodownWPShortcodeSettings();
+} else {
+	new UptodownWPShortcode();
+}
+
+?>
